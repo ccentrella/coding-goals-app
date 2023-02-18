2ab7ec0265205a4bb813d9294c14d6c9f677a2f2
Christopher Centrella <ccentrella.j@gmail.com>
2023-02-17 19:36:49 -0600

    Initial Commit

---
+++ Coding Goals App/Coding_Goals_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coding_Goals_AppApp.swift
+//  Coding Goals App
+//
+//  Created by Christopher Centrella on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Coding_Goals_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
