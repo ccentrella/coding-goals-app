7eda18cca20cc50e613737a8b2e4d91f23761adf
Christopher Centrella <ccentrella.j@gmail.com>
2023-02-28 17:36:40 -0600

    Update GoalDetail view

--- Coding Goals App/Views/Goal/GoalDetail.swift
+++ Coding Goals App/Views/Goal/GoalDetail.swift
@@ -11,15 +11,67 @@ struct GoalDetail: View {
     
     let goal: Goal
     var body: some View {
-        Text(goal.description)
+        VStack(spacing: 15) {
+            HStack(spacing: 15) {
+                ZStack {
+                    Circle()
+                        .frame(height: 60)
+                    Image(goalType: goal.type)
+                        .foregroundColor(goal.type.getColor())
+                }
+                Text(goal.description)
+                    .bold()
+                Spacer()
+            }
+            .font(.system(.title2))
+            .padding(.top, 5.0)
+            .padding(.bottom, 10.0)
+            
+            HStack {
+                if goal.deadline.hasValue {
+                    Label("Due", systemImage: "clock")
+                    Text(goal.deadline.date.description)
+                }
+                else {
+                    Label("No deadline specified", systemImage: "clock")
+                }
+                Spacer()
+            }
+        
+            HStack {
+                Text(InspirationalMessage.getInspirationalMessage(goal: goal))
+                    .font(.headline)
+                    .padding(.top, 5.0)
+                Spacer()
+            }
+            if !goal.notes.isEmpty {
+                HStack {
+                    Text("Notes: \(goal.notes)")
+                    Spacer()
+                }
+            }
+            Spacer()
+        }
+        .toolbar {
+            EditButton()
+        }
+        .navigationTitle("Explore Goal")
+        .toolbarColorScheme(.dark, for: .navigationBar)
+        .toolbarBackground(goal.type.getColor(), for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
+        .padding()
+        .background(goal.type.getColor())
+        .foregroundColor(.white)
     }
 }
 
-struct DetailView_Previews: PreviewProvider {
+struct GoalDetail_Previews: PreviewProvider {
     static var previews: some View {
         let dataStore = DataStore()
         
-        GoalDetail(goal: dataStore.goals[0])
-            .environmentObject(dataStore)
+        NavigationStack {
+            GoalDetail(goal: dataStore.goals[0])
+                .environmentObject(dataStore)
+        }
     }
 }
