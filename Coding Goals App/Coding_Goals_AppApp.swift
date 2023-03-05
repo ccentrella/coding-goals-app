8a072061dc3ef363cab02d82fe70d438fc7d00bc
Christopher Centrella <ccentrella.j@gmail.com>
2023-03-04 21:08:12 -0600

    Add functionality to persist and retrieve goals.

--- Coding Goals App/Coding_Goals_AppApp.swift
+++ Coding Goals App/Coding_Goals_AppApp.swift
@@ -11,11 +11,22 @@ import SwiftUI
 struct Coding_Goals_AppApp: App {
     
     @StateObject private var dataStore = DataStore()
+    @Environment(\.scenePhase) private var scenePhase
     
     var body: some Scene {
         WindowGroup {
             GoalListView()
                 .environmentObject(dataStore)
+                .onAppear {
+                    dataStore.loadGoals()
+                }
+                .onChange(of: scenePhase) { phase in
+                    if phase == .inactive {
+                        dataStore.saveGoals()
+                    }
+                }
         }
     }
+    
+    
 }
--- Coding Goals App/Model/DataStore.swift
+++ Coding Goals App/Model/DataStore.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 
 public final class DataStore: ObservableObject {
-    @Published var goals: [Goal] = load("sampleGoals.json")
+    @Published var goals: [Goal] = []
     
     func getBinding(goal: Goal) -> Binding<Goal> {
         return Binding<Goal> {
@@ -19,35 +19,108 @@ public final class DataStore: ObservableObject {
             self.goals[goalIndex] = newValue
         }
     }
-}
-
-func load<T: Decodable> (_ filename: String) -> T {
-    let file: URL = getFile(filename)
-    let data: Data = fetchJson(filename, file: file)
-    return decodeJson(filename, data: data)
-}
-
-private func getFile(_ filename: String) -> URL {
-    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
+    
+    func loadGoals() {
+        DataStore.load { result in
+            switch (result) {
+            case .failure(let error):
+                fatalError(error.localizedDescription)
+            case .success(let goals):
+                self.goals = goals
+            }
+        }
     }
-    return file
-}
-
-private func fetchJson(_ filename: String, file: URL) -> Data {
-    do {
-        return try Data(contentsOf: file)
-    } catch {
-        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    
+    func saveGoals() {
+        DataStore.save(goals: self.goals) { result in
+            if case.failure(let error) = result {
+                fatalError(error.localizedDescription)
+            }
+        }
     }
-}
-
-private func decodeJson<T: Decodable> (_ filename: String, data: Data) -> T {
-    do {
-        let decoder = JSONDecoder()
-        return try decoder.decode(T.self, from: data)
-    } catch {
-        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    
+    private static func load(completion: @escaping (Result<[Goal], Error>) -> Void) {
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                let fileURL = try DataStore.fileURL()
+                guard let file = try? FileHandle(forReadingFrom: fileURL) else {
+                    DispatchQueue.main.async {
+                        completion(.success([]))
+                    }
+                    return
+                }
+                let goals = try JSONDecoder().decode([Goal].self, from: file.availableData)
+                DispatchQueue.main.async {
+                    completion(.success(goals))
+                }
+            }
+            catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
     }
+    
+    private static func save(goals: [Goal], completion: @escaping (Result<Int, Error>) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                try writeToFile(goals: goals)
+                DispatchQueue.main.async {
+                    completion(.success(goals.count))
+                }
+            }
+            catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    
+    private static func writeToFile(goals: [Goal]) throws {
+        let data = try JSONEncoder().encode(goals)
+        let fileURL = try fileURL()
+        try data.write(to: fileURL)
+    }
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
+    }
+    
+    func load<T: Decodable> (_ filename: String) -> T {
+        let file: URL = getFile(filename)
+        let data: Data = fetchJson(filename, file: file)
+        return decodeJson(filename, data: data)
+    }
+    
+    private func getFile(_ filename: String) -> URL {
+        guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+        }
+        return file
+    }
+    
+    private func fetchJson(_ filename: String, file: URL) -> Data {
+        do {
+            return try Data(contentsOf: file)
+        } catch {
+            fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+        }
+    }
+    
+    private func decodeJson<T: Decodable> (_ filename: String, data: Data) -> T {
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+        }
+    }
+    
 }
