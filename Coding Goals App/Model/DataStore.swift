ff03428e3719b5c12721cd4d08bc8a612b204b3c
Christopher Centrella <ccentrella.j@gmail.com>
2023-03-04 22:36:45 -0600

    Modify DataStore to allow sample data to load during preview

--- Coding Goals App/Model/DataStore.swift
+++ Coding Goals App/Model/DataStore.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 
 public final class DataStore: ObservableObject {
-    @Published var goals: [Goal] = []
+    @Published var goals: [Goal] = loadSampleGoals("sampleGoals.json")
     
     func getBinding(goal: Goal) -> Binding<Goal> {
         return Binding<Goal> {
@@ -91,36 +91,33 @@ public final class DataStore: ObservableObject {
                                     create: false)
         .appendingPathComponent("scrums.data")
     }
-    
-    func load<T: Decodable> (_ filename: String) -> T {
-        let file: URL = getFile(filename)
-        let data: Data = fetchJson(filename, file: file)
-        return decodeJson(filename, data: data)
+}
+
+func loadSampleGoals<T: Decodable> (_ filename: String) -> T {
+    let file: URL = getFile(filename)
+    let data: Data = fetchJson(filename, file: file)
+    return decodeJson(filename, data: data)
+}
+
+private func getFile(_ filename: String) -> URL {
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
-    private func getFile(_ filename: String) -> URL {
-        guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-        else {
-            fatalError("Couldn't find \(filename) in main bundle.")
-        }
-        return file
+    return file
+}
+private func fetchJson(_ filename: String, file: URL) -> Data {
+    do {
+        return try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
-    private func fetchJson(_ filename: String, file: URL) -> Data {
-        do {
-            return try Data(contentsOf: file)
-        } catch {
-            fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
-        }
+}
+private func decodeJson<T: Decodable> (_ filename: String, data: Data) -> T {
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
-    
-    private func decodeJson<T: Decodable> (_ filename: String, data: Data) -> T {
-        do {
-            let decoder = JSONDecoder()
-            return try decoder.decode(T.self, from: data)
-        } catch {
-            fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
-        }
-    }
-    
 }
