39faf106ac2acc1b472e0716e740143cee90d36d
Christopher Centrella <ccentrella.j@gmail.com>
2023-04-19 19:15:16 -0500

    Update NotificationService to use Apple's UNUserNotificationCenter API

--- CodingGoals/NotificationService/NotificationService.swift
+++ CodingGoals/NotificationService/NotificationService.swift
@@ -6,102 +6,127 @@
 //
 
 import Foundation
+import UserNotifications;
 
 class NotificationService {
     
-    static var goalsDueNow: [GoalNotification] = []
-    static var goalsDueToday: [GoalNotification] = []
-    static var goalsDueSoon: [GoalNotification] = []
-    static var goalsDueThisWeek: [GoalNotification] = []
-    static var goalsDueLater: [GoalNotification] = []
+    private static let calendar: Calendar = Calendar(identifier: .gregorian)
     
-    static func addOrUpdateGoal(goalNotification: GoalNotification) {
+    public static func addGoal(goal: Goal) async throws {
         
-        if isDueNow(goalNotification) {
-            goalsDueNow.append(goalNotification)
-            goalsDueNow.sort(by: >)
-        }
-        else if isDueToday(goalNotification) {
-            goalsDueToday.append(goalNotification)
-            goalsDueToday.sort(by: >)
-        }
-        else if isDueSoon(goalNotification) {
-            goalsDueSoon.append(goalNotification)
-            goalsDueSoon.sort(by: >)
-        }
-        else if isDueThisWeek(goalNotification) {
-            goalsDueThisWeek.append(goalNotification)
-            goalsDueThisWeek.sort(by: >)
-        }
-        else {
-            goalsDueLater.append(goalNotification)
-            goalsDueLater.sort(by: >)
+        if (goal.overview.deadline.hasValue) {
+            try await addDeadlineNotification(goal: goal)
+            try await addAlertNotification(goal: goal)
+            try await addRemindMeNotification(goal: goal)
         }
     }
     
-    static func removeGoal(goalId: UUID) {
-        goalsDueNow.removeAll(where: { goal in goal.goalId == goalId })
-        goalsDueToday.removeAll(where: { goal in goal.goalId == goalId })
-        goalsDueSoon.removeAll(where: { goal in goal.goalId == goalId })
-        goalsDueThisWeek.removeAll(where: { goal in goal.goalId == goalId })
-        goalsDueLater.removeAll(where: { goal in goal.goalId == goalId })
-    }
-    
-    static private func weeklyTimer() {
-        var goalNotification = goalsDueLater.last
-        while goalNotification != nil && isDueThisWeek(goalNotification!) {
-            goalsDueThisWeek.append(goalNotification!)
-            goalsDueLater.removeLast()
-            goalNotification = goalsDueLater.last
-        }
-    }
-    static private func semiWeeklyTimer() {
-        var goalNotification = goalsDueThisWeek.last
-        while goalNotification != nil && isDueSoon(goalNotification!) {
-            goalsDueSoon.append(goalNotification!)
-            goalsDueThisWeek.removeLast()
-            goalNotification = goalsDueThisWeek.last
-        }
-    }
-    static private func dailyTimer() {
-        var goalNotification = goalsDueSoon.last
-        while goalNotification != nil && isDueToday(goalNotification!) {
-            goalsDueToday.append(goalNotification!)
-            goalsDueSoon.removeLast()
-            goalNotification = goalsDueSoon.last
-        }
-    }
-    static private func minuteTimer() {
-        var goalNotification = goalsDueToday.last
-        while goalNotification != nil && isDueNow(goalNotification!) {
-            goalsDueNow.append(goalNotification!)
-            goalsDueToday.removeLast()
-            goalNotification = goalsDueToday.last
-        }
-    }
-    
-    static private func isDueThisWeek(_ goalNotification: GoalNotification) -> Bool {
-        let timeRemaining = getTimeRemaining(date: goalNotification.dueDate)
-        return timeRemaining <= TimeIntervalConstants.week
-    }
-    static private func isDueSoon(_ goalNotification: GoalNotification) -> Bool {
-        let timeRemaining = getTimeRemaining(date: goalNotification.dueDate)
-        return timeRemaining <= TimeIntervalConstants.semiWeek
-    }
-    static private func isDueToday(_ goalNotification: GoalNotification) -> Bool {
-        let timeRemaining = getTimeRemaining(date: goalNotification.dueDate)
-        return timeRemaining <= TimeIntervalConstants.day
-    }
-    static private func isDueNow(_ goalNotification: GoalNotification) -> Bool {
-        let timeRemaining = getTimeRemaining(date: goalNotification.dueDate)
-        return timeRemaining <= TimeIntervalConstants.minute
-    }
-    
-    static private func getTimeRemaining(date: Date) -> TimeInterval {
-        return date.distance(to: Date.now)
-    }
-    
-    static private func showNotification() {
+    public static func removeGoal(goalId: UUID) {
         
+        let deadline_identifier = "\(goalId)_deadline"
+        let alert_identifier = "\(goalId)_alert"
+        let remindme_identifier = "\(goalId)_remindme"
+        let identifiers: [String] = [deadline_identifier, alert_identifier, remindme_identifier]
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.removePendingNotificationRequests(withIdentifiers: identifiers)
+    }
+    
+    private static func addDeadlineNotification(goal: Goal) async throws {
+        
+        let date: Date = goal.overview.deadline.date
+        try await addNotification(goal: goal, date: date, notification: "deadline")
+    }
+    
+    private static func addAlertNotification(goal: Goal) async throws {
+        
+        var date: Date
+        switch (goal.notifications.alert) {
+            case .none:
+                return
+            case .fifteenminutesbefore:
+                date = goal.overview.deadline.date - 900
+            case .thirtyminutesbefore:
+                date = goal.overview.deadline.date - 1800
+            case .onehourbefore:
+                date = goal.overview.deadline.date - 3600
+            case .twohoursbefore:
+                date = goal.overview.deadline.date - 7200
+        }
+        
+        try await addNotification(goal: goal, date: date, notification: "alert")
+    }
+    
+    private static func addRemindMeNotification(goal: Goal) async throws {
+        
+        var date: Date = goal.overview.deadline.date
+        switch (goal.notifications.remindMe) {
+            case .none:
+                return
+            case .inmorning:
+                date = getMorningDate(date: date)
+            case .inevening:
+                date = getEveningDate(date: date)
+            case .fifteenminutesbefore:
+                date = goal.overview.deadline.date - 900
+            case .thirtyminutesbefore:
+                date = goal.overview.deadline.date - 1800
+            case .onehourbefore:
+                date = goal.overview.deadline.date - 3600
+            case .twohoursbefore:
+                date = goal.overview.deadline.date - 7200
+            case .onedaybefore:
+                date = goal.overview.deadline.date - 86400
+            case .twodaysbefore:
+                date = goal.overview.deadline.date - 172800
+            case .oneweekbefore:
+                date = goal.overview.deadline.date - 604800
+        }
+        
+        try await addNotification(goal: goal, date: date, notification: "remindme")
+    }
+    
+    private static func addNotification(goal: Goal, date: Date, notification: String) async throws {
+        
+        var dateComponents: DateComponents = calendar.dateComponents(in: .autoupdatingCurrent, from: date)
+        var content: UNNotificationContent = UNNotificationContent()
+        var trigger: UNCalendarNotificationTrigger = UNCalendarNotificationTrigger(
+            dateMatching: dateComponents,
+            repeats: false)
+        var request: UNNotificationRequest = UNNotificationRequest(
+            identifier: "\(goal.id.uuidString)_\(notification)",
+            content: content,
+            trigger: trigger)
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        try await notificationCenter.add(request)
+    }
+    
+    private static func getMorningDate(date: Date) -> Date {
+        
+        var dateComponents: DateComponents = calendar.dateComponents(in: .autoupdatingCurrent, from: date)
+        if (dateComponents.hour! < 8) {
+            dateComponents.day! -= 1
+        }
+        
+            dateComponents.hour = 8
+            dateComponents.minute = 0
+            dateComponents.second = 0
+        
+        return dateComponents.date!
+    }
+    
+    private static func getEveningDate(date: Date) -> Date {
+        
+        var dateComponents: DateComponents = calendar.dateComponents(in: .autoupdatingCurrent, from: date)
+        if (dateComponents.hour! < 17) {
+            dateComponents.day! -= 1
+        }
+        
+            dateComponents.hour = 17
+            dateComponents.minute = 0
+            dateComponents.second = 0
+        
+        return dateComponents.date!
     }
 }
