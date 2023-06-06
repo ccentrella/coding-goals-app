e706c56304f5724795e3da99968aa1a97c839f9a
Christopher Centrella <ccentrella.j@gmail.com>
2023-06-05 22:04:22 -0500

    Create test cases for extensions

--- CodingGoalsTests/Extensions/ImageExtension.swift
+++ CodingGoalsTests/Extensions/ImageExtension.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import XCTest
+import SwiftUI
+@testable import CodingGoals
+
+final class ImageExtensionTest: XCTestCase {
+    
+    func testGoalTypeImageConstructor() throws {
+        
+        let pages = Image(goalType: .pages)
+        let books = Image(goalType: .books)
+        let videos = Image(goalType: .videos)
+        let functions = Image(goalType: .functions)
+        let articles = Image(goalType: .articles)
+        let apps = Image(goalType: .apps)
+        
+        assert(pages == Image(systemName: "doc.plaintext"), "wrong system image being returned for pages")
+        assert(books == Image(systemName: "book"), "wrong system image being returned for books")
+        assert(videos == Image(systemName: "video"), "wrong system image being returned for videos")
+        assert(functions == Image(systemName: "function"), "wrong system image being returned for functions")
+        assert(articles == Image(systemName: "newspaper"), "wrong system image being returned for articles")
+        assert(apps == Image(systemName: "apps.iphone"), "wrong system image being returned for apps")
+
+    }
+}
--- CodingGoalsTests/Extensions/StringExtension.swift
+++ CodingGoalsTests/Extensions/StringExtension.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+@testable import CodingGoals
+import XCTest
+
+final class StringExtensionTest: XCTestCase {
+    
+    func testSingularize() throws {
+        assert("Tests".singularize(count: 0) == "Tests", "string should be plural")
+        assert("Tests".singularize(count: 1) == "Test", "string should be singular")
+        assert("Tests".singularize(count: 2) == "Tests", "string should be plural")
+    }
+}
