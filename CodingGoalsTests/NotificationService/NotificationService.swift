da07d7ff7956592f25f6d795db7b1eb47041f027
Christopher Centrella <ccentrella.j@gmail.com>
2023-07-16 15:52:51 -0500

    Uncomment code and add comment to clarify issue that can cause tests to fail

--- CodingGoalsTests/NotificationService/NotificationService.swift
+++ CodingGoalsTests/NotificationService/NotificationService.swift
@@ -5,6 +5,8 @@
 //  Created by Christopher Centrella on 4/19/23.
 //
 
+// Requires notifications permission for test to pass
+
 import Foundation
 @testable import CodingGoals
 import XCTest
@@ -38,124 +40,124 @@ final class NotificationServiceTest: XCTestCase {
         assert(pendingRequests.count == 0, "Notifications should only be added if goal has deadline")
     }
     
-//    func testAddGoalWithNonRepeatingDeadlineOnly() async throws {
-//        let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
-//        let deadline = NullableDate(hasValue: true, date: Date.now.advanced(by: TimeInterval(60)))
-//        let overview: GoalOverview = GoalOverview(description: "Test description", type: .apps, length: 5, deadline: deadline)
-//        let `repeat`: GoalRepeat = GoalRepeat(repeatOption: .never)
-//        let notifications: GoalNotifications = GoalNotifications()
-//        let notes: String = "Some sample notes"
-//        let data: Goal.Data = Goal.Data(overview: overview, repeat: `repeat`, notifications: notifications, notes: notes)
-//
-//        let goal: Goal = Goal.init(data: data)
-//        try await NotificationService.addGoal(goal: goal)
-//        let pendingRequests = await notificationCenter.pendingNotificationRequests()
-//
-//        assert(pendingRequests.count != 0, "Notifications not added successfully.")
-//        assert(pendingRequests.count == 1, "Wrong number of notifications added.")
-//
-//        let notification: UNNotificationRequest = pendingRequests.first!
-//        testDeadlineNotification(goal: goal, notification: notification)
-//    }
+    func testAddGoalWithNonRepeatingDeadlineOnly() async throws {
+        let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
+        let deadline = NullableDate(hasValue: true, date: Date.now.advanced(by: TimeInterval(60)))
+        let overview: GoalOverview = GoalOverview(description: "Test description", type: .apps, length: 5, deadline: deadline)
+        let `repeat`: GoalRepeat = GoalRepeat(repeatOption: .never)
+        let notifications: GoalNotifications = GoalNotifications()
+        let notes: String = "Some sample notes"
+        let data: Goal.Data = Goal.Data(overview: overview, repeat: `repeat`, notifications: notifications, notes: notes)
+
+        let goal: Goal = Goal.init(data: data)
+        try await NotificationService.addGoal(goal: goal)
+        let pendingRequests = await notificationCenter.pendingNotificationRequests()
+
+        assert(pendingRequests.count != 0, "Notifications not added successfully.")
+        assert(pendingRequests.count == 1, "Wrong number of notifications added.")
+
+        let notification: UNNotificationRequest = pendingRequests.first!
+        testDeadlineNotification(goal: goal, notification: notification)
+    }
     
-//    func testAddGoalWithNonRepeatingAlert() async throws {
-//        let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
-//        let deadline = NullableDate(hasValue: true, date: Date.now.advanced(by: TimeInterval(60)))
-//        let overview: GoalOverview = GoalOverview(description: "Test description", type: .apps, length: 5, deadline: deadline)
-//        let `repeat`: GoalRepeat = GoalRepeat(repeatOption: .never)
-//        let notifications: GoalNotifications = GoalNotifications(remindMe: .none, alert: .fifteenminutesbefore, showAlertBanner: true, showCongratsBanner: true)
-//        let notes: String = "Some sample notes"
-//        let data: Goal.Data = Goal.Data(overview: overview, repeat: `repeat`, notifications: notifications, notes: notes)
-//
-//        let goal: Goal = Goal.init(data: data)
-//        try await NotificationService.addGoal(goal: goal)
-//        let pendingRequests = await notificationCenter.pendingNotificationRequests()
-//
-//        assert(pendingRequests.count != 0, "Notifications not added successfully.")
-//        assert(pendingRequests.count != 1, "Only one notification was added. Two notifications should have been added, a deadline notification and an alert notification.")
-//        assert(pendingRequests.count == 2, "Wrong number of notifications added. Two notifications should have been added, a deadline notification and an alert notification.")
-//
-//        let first_notification: UNNotificationRequest = pendingRequests[0]
-//        let second_notification: UNNotificationRequest = pendingRequests[1]
-//        testDeadlineNotification(goal: goal, notification: first_notification)
-//        testAlertNotification(goal: goal, notification: second_notification)
-//    }
-//
-//    func testAddGoalWithNonRepeatingRemindMe() async throws {
-//        let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
-//        let deadline = NullableDate(hasValue: true, date: Date.now.advanced(by: TimeInterval(60)))
-//        let overview: GoalOverview = GoalOverview(description: "Test description", type: .apps, length: 5, deadline: deadline)
-//        let `repeat`: GoalRepeat = GoalRepeat(repeatOption: .never)
-//        let notifications: GoalNotifications = GoalNotifications(remindMe: .fifteenminutesbefore, alert: .none, showAlertBanner: true, showCongratsBanner: true)
-//        let notes: String = "Some sample notes"
-//        let data: Goal.Data = Goal.Data(overview: overview, repeat: `repeat`, notifications: notifications, notes: notes)
-//
-//        let goal: Goal = Goal.init(data: data)
-//        try await NotificationService.addGoal(goal: goal)
-//        let pendingRequests = await notificationCenter.pendingNotificationRequests()
-//
-//        assert(pendingRequests.count != 0, "Notifications not added successfully.")
-//        assert(pendingRequests.count != 1, "Only one notification was added. Two notifications should have been added, a deadline notification and a remindme notification.")
-//        assert(pendingRequests.count == 2, "Wrong number of notifications added. Two notifications should have been added, a deadline notification and a remindme notification.")
-//
-//        let first_notification: UNNotificationRequest = pendingRequests[0]
-//        let second_notification: UNNotificationRequest = pendingRequests[1]
-//        testDeadlineNotification(goal: goal, notification: first_notification)
-//        testRemindMeNotification(goal: goal, notification: second_notification)
-//    }
+    func testAddGoalWithNonRepeatingAlert() async throws {
+        let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
+        let deadline = NullableDate(hasValue: true, date: Date.now.advanced(by: TimeInterval(60)))
+        let overview: GoalOverview = GoalOverview(description: "Test description", type: .apps, length: 5, deadline: deadline)
+        let `repeat`: GoalRepeat = GoalRepeat(repeatOption: .never)
+        let notifications: GoalNotifications = GoalNotifications(remindMe: .none, alert: .fifteenminutesbefore, showAlertBanner: true, showCongratsBanner: true)
+        let notes: String = "Some sample notes"
+        let data: Goal.Data = Goal.Data(overview: overview, repeat: `repeat`, notifications: notifications, notes: notes)
+
+        let goal: Goal = Goal.init(data: data)
+        try await NotificationService.addGoal(goal: goal)
+        let pendingRequests = await notificationCenter.pendingNotificationRequests()
+
+        assert(pendingRequests.count != 0, "Notifications not added successfully.")
+        assert(pendingRequests.count != 1, "Only one notification was added. Two notifications should have been added, a deadline notification and an alert notification.")
+        assert(pendingRequests.count == 2, "Wrong number of notifications added. Two notifications should have been added, a deadline notification and an alert notification.")
+
+        let first_notification: UNNotificationRequest = pendingRequests[0]
+        let second_notification: UNNotificationRequest = pendingRequests[1]
+        testDeadlineNotification(goal: goal, notification: first_notification)
+        testAlertNotification(goal: goal, notification: second_notification)
+    }
+
+    func testAddGoalWithNonRepeatingRemindMe() async throws {
+        let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
+        let deadline = NullableDate(hasValue: true, date: Date.now.advanced(by: TimeInterval(60)))
+        let overview: GoalOverview = GoalOverview(description: "Test description", type: .apps, length: 5, deadline: deadline)
+        let `repeat`: GoalRepeat = GoalRepeat(repeatOption: .never)
+        let notifications: GoalNotifications = GoalNotifications(remindMe: .fifteenminutesbefore, alert: .none, showAlertBanner: true, showCongratsBanner: true)
+        let notes: String = "Some sample notes"
+        let data: Goal.Data = Goal.Data(overview: overview, repeat: `repeat`, notifications: notifications, notes: notes)
+
+        let goal: Goal = Goal.init(data: data)
+        try await NotificationService.addGoal(goal: goal)
+        let pendingRequests = await notificationCenter.pendingNotificationRequests()
+
+        assert(pendingRequests.count != 0, "Notifications not added successfully.")
+        assert(pendingRequests.count != 1, "Only one notification was added. Two notifications should have been added, a deadline notification and a remindme notification.")
+        assert(pendingRequests.count == 2, "Wrong number of notifications added. Two notifications should have been added, a deadline notification and a remindme notification.")
+
+        let first_notification: UNNotificationRequest = pendingRequests[0]
+        let second_notification: UNNotificationRequest = pendingRequests[1]
+        testDeadlineNotification(goal: goal, notification: first_notification)
+        testRemindMeNotification(goal: goal, notification: second_notification)
+    }
     
-//    func testAddGoalWithNonRepeatingAlertAndRemindMe() async throws {
-//        let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
-//        let deadline = NullableDate(hasValue: true, date: Date.now.advanced(by: TimeInterval(60)))
-//        let overview: GoalOverview = GoalOverview(description: "Test description", type: .apps, length: 5, deadline: deadline)
-//        let `repeat`: GoalRepeat = GoalRepeat(repeatOption: .never)
-//        let notifications: GoalNotifications = GoalNotifications(remindMe: .fifteenminutesbefore, alert: .none, showAlertBanner: true, showCongratsBanner: true)
-//        let notes: String = "Some sample notes"
-//        let data: Goal.Data = Goal.Data(overview: overview, repeat: `repeat`, notifications: notifications, notes: notes)
-//
-//        let goal: Goal = Goal.init(data: data)
-//        try await NotificationService.addGoal(goal: goal)
-//        let pendingRequests = await notificationCenter.pendingNotificationRequests()
-//
-//        assert(pendingRequests.count != 0, "Notifications not added successfully.")
-//        assert(pendingRequests.count != 1, "Only one notification was added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
-//        assert(pendingRequests.count != 2, "Only two notifications were added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
-//        assert(pendingRequests.count == 3, "Wrong number of notifications added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
-//
-//        let first_notification: UNNotificationRequest = pendingRequests[0]
-//        let second_notification: UNNotificationRequest = pendingRequests[1]
-//        let third_notification: UNNotificationRequest = pendingRequests[2]
-//        testDeadlineNotification(goal: goal, notification: first_notification)
-//        testAlertNotification(goal: goal, notification: second_notification)
-//        testRemindMeNotification(goal: goal, notification: third_notification)
-//    }
-//
-//    func testAddGoalWithRepeatingAlertAndRemindMe() async throws {
-//        let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
-//        let deadline = NullableDate(hasValue: true, date: Date.now.advanced(by: TimeInterval(60)))
-//        let overview: GoalOverview = GoalOverview(description: "Test description", type: .apps, length: 5, deadline: deadline)
-//        let `repeat`: GoalRepeat = GoalRepeat(repeatOption: .everyday)
-//        let notifications: GoalNotifications = GoalNotifications(remindMe: .fifteenminutesbefore, alert: .none, showAlertBanner: true, showCongratsBanner: true)
-//        let notes: String = "Some sample notes"
-//        let data: Goal.Data = Goal.Data(overview: overview, repeat: `repeat`, notifications: notifications, notes: notes)
-//
-//        let goal: Goal = Goal.init(data: data)
-//        try await NotificationService.addGoal(goal: goal)
-//        let pendingRequests = await notificationCenter.pendingNotificationRequests()
-//
-//        assert(pendingRequests.count != 0, "Notifications not added successfully.")
-//        assert(pendingRequests.count != 1, "Only one notification was added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
-//        assert(pendingRequests.count != 2, "Only two notifications were added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
-//        assert(pendingRequests.count == 3, "Wrong number of notifications added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
-//
-//        let first_notification: UNNotificationRequest = pendingRequests[0]
-//        let second_notification: UNNotificationRequest = pendingRequests[1]
-//        let third_notification: UNNotificationRequest = pendingRequests[2]
-//        testDeadlineNotification(goal: goal, notification: first_notification)
-//        testAlertNotification(goal: goal, notification: second_notification)
-//        testRemindMeNotification(goal: goal, notification: third_notification)
-//    }
-//
+    func testAddGoalWithNonRepeatingAlertAndRemindMe() async throws {
+        let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
+        let deadline = NullableDate(hasValue: true, date: Date.now.advanced(by: TimeInterval(60)))
+        let overview: GoalOverview = GoalOverview(description: "Test description", type: .apps, length: 5, deadline: deadline)
+        let `repeat`: GoalRepeat = GoalRepeat(repeatOption: .never)
+        let notifications: GoalNotifications = GoalNotifications(remindMe: .fifteenminutesbefore, alert: .none, showAlertBanner: true, showCongratsBanner: true)
+        let notes: String = "Some sample notes"
+        let data: Goal.Data = Goal.Data(overview: overview, repeat: `repeat`, notifications: notifications, notes: notes)
+
+        let goal: Goal = Goal.init(data: data)
+        try await NotificationService.addGoal(goal: goal)
+        let pendingRequests = await notificationCenter.pendingNotificationRequests()
+
+        assert(pendingRequests.count != 0, "Notifications not added successfully.")
+        assert(pendingRequests.count != 1, "Only one notification was added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
+        assert(pendingRequests.count != 2, "Only two notifications were added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
+        assert(pendingRequests.count == 3, "Wrong number of notifications added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
+
+        let first_notification: UNNotificationRequest = pendingRequests[0]
+        let second_notification: UNNotificationRequest = pendingRequests[1]
+        let third_notification: UNNotificationRequest = pendingRequests[2]
+        testDeadlineNotification(goal: goal, notification: first_notification)
+        testAlertNotification(goal: goal, notification: second_notification)
+        testRemindMeNotification(goal: goal, notification: third_notification)
+    }
+
+    func testAddGoalWithRepeatingAlertAndRemindMe() async throws {
+        let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
+        let deadline = NullableDate(hasValue: true, date: Date.now.advanced(by: TimeInterval(60)))
+        let overview: GoalOverview = GoalOverview(description: "Test description", type: .apps, length: 5, deadline: deadline)
+        let `repeat`: GoalRepeat = GoalRepeat(repeatOption: .everyday)
+        let notifications: GoalNotifications = GoalNotifications(remindMe: .fifteenminutesbefore, alert: .none, showAlertBanner: true, showCongratsBanner: true)
+        let notes: String = "Some sample notes"
+        let data: Goal.Data = Goal.Data(overview: overview, repeat: `repeat`, notifications: notifications, notes: notes)
+
+        let goal: Goal = Goal.init(data: data)
+        try await NotificationService.addGoal(goal: goal)
+        let pendingRequests = await notificationCenter.pendingNotificationRequests()
+
+        assert(pendingRequests.count != 0, "Notifications not added successfully.")
+        assert(pendingRequests.count != 1, "Only one notification was added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
+        assert(pendingRequests.count != 2, "Only two notifications were added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
+        assert(pendingRequests.count == 3, "Wrong number of notifications added. Three notifications should have been added, a deadline notification, an alert notification and a remindme notification.")
+
+        let first_notification: UNNotificationRequest = pendingRequests[0]
+        let second_notification: UNNotificationRequest = pendingRequests[1]
+        let third_notification: UNNotificationRequest = pendingRequests[2]
+        testDeadlineNotification(goal: goal, notification: first_notification)
+        testAlertNotification(goal: goal, notification: second_notification)
+        testRemindMeNotification(goal: goal, notification: third_notification)
+    }
+
     func testRemoveGoal() async throws {
         let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
         let store: DataStore = DataStore()
